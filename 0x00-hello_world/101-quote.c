49bdba9f088d983c35bf5b59600f8a35c00b9a40
gromok-alx <ok.gravin.com>
2023-03-16 09:18:46 -0700

    101 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,9 @@
 int main(void)
 {
 	char Mystring[100];
-	sprintf(Mystring, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	sprintf(Mystring, "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19");
 	printf("%s\n", Mystring);
 
 	return (1);
