aa03f818501bcc55a1368fe7f6a0eb30534a8a18
gromok-alx <ok.gravin.com>
2023-03-16 07:10:48 -0700

    printing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*5-printf.c: prints a string using the printf function*/
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print out a string using printf function'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
