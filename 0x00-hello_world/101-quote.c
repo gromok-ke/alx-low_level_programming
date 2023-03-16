9c17f10fdfb11e118f86e60edadf087d7c8e3360
gromok-alx <ok.gravin.com>
2023-03-16 08:56:25 -0700

    project 100-intel and 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+/*101-quote.c:printing a string without puts or printf*/
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program will print out a value'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char Mystring[100];
+	sprintf(Mystring, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("%s\n", Mystring);
+
+	return (1);
+}
