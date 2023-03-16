e27702ab4df7e5a854ccf6a9adba2fb1c2e34a08
gromok-alx <ok.gravin.com>
2023-03-16 06:25:17 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*4-puts.c: this code prints out an ouput when run*/
+
+#include <stdio.h>
+/**
+ * main() will run all the programs
+ * that are enclosed inside it.
+ *
+ * return ends the execution of the program
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
