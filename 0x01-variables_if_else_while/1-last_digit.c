15c1578d8ef4271fe1bdd960f62f303d220340ea
gromok-alx <ok.gravin.com>
2023-03-17 11:40:38 -0700

    using AND and if, else if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,21 +11,23 @@
 
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	last_digit = n % 10;
 
 	if (n > 5)
-		printf("Last digit of n is %d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater"
+			"than 5\n", n, last_digit);
 
 	else if (n == 0)
-		printf("Last digit of n is %d and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 
 	else if (n < 6 && n != 0)
-		printf("Last digit of n is %d and is less"
-			"than 6 and not 0\n", n);
+		printf("Last digit of %n is %d and is less"
+			"than 6 and not 0\n", n, last_digit);
 
 	return (0);
 }
