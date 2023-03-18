f19fcba231c4c1384612f53aab3c724e6edba15c
gromok-alx <ok.gravin.com>
2023-03-18 01:21:13 -0700

    printing all characters lowercase exept q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * int - Entry point of the program
+ *
+ * Description: Program to print lowercase alphabet a-z except q & e
+ *
+ * Return: End point to the program. should be 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	/*print all characters except q and e*/
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
