7cf2a031c0eae90436fafd2848641a52ae620328
gromok-alx <ok.gravin.com>
2023-03-20 08:17:00 -0700

    last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, y);
 
 	else if (y < 6 && y != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y;
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 
 	return (0);
 }
