d0db95d51eaf03cd5c177854decd1f0b3ecc1557
gromok-alx <ok.gravin.com>
2023-03-21 10:22:05 -0700

    commit for 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,3 @@
-#include <unistd.h>
-
-/**
- * _putchar - prints a single character to the console
- *
- * Description: This code defines implementation of _putchar function
- *
- * @c: parameter that is taken as input character in _putchar
- *
- * Return: returns number of bytes written by write function
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 /**
  * print_alphabet_x10 - function to print alphabets 10x when called
  *
