318f7e9c4ba3eae5ac6c3983486741f1fcbe36ad
gromok-alx <ok.gravin.com>
2023-03-21 11:13:44 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,4 @@
-#include <unistd.h>
-
-/**
- * _putchar - prints a single character to the console
- *
- * Description: This code defines implementation of _putchar function
- *
- * @c: parameter that is taken as input character in _putchar
- *
- * Return: returns number of bytes written by write function
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+#include "main.h"
 /**
  * print_alphabet - function to print alphabets when called
  *
