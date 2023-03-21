7b5266626eed848b5ea9077e9609387ed3c4eb2d
gromok-alx <ok.gravin.com>
2023-03-21 12:10:11 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+/**
+ * _isalpha - checks if value is an alphabet.
+ *
+ * @c: value to be checked
+ *
+ * Return: 1 if true 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
