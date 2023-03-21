dba13b85093fec401e072a751d99df5d84da3265
gromok-alx <ok.gravin.com>
2023-03-21 13:35:55 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,5 @@
+#incude "main.h"
+
 /**
  * print_sign - prints the sign of an int either +, - or 0
  *
@@ -10,21 +12,21 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf('+');
+		_putchar('+');
 
 		return (1);
 	}
 
 	else if (n == 0)
 	{
-		printf('0');
+		_putchar('0');
 
 		return (0);
 	}
 
 	else if (n < 0)
 	{
-		printf('-');
+		_putchar('-');
 
 		return (-1);
 	}
