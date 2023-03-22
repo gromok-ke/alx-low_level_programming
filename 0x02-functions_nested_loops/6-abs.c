6ce4ffe26d180d426923482a86f73abafe52279d
gromok-alx <ok.gravin.com>
2023-03-22 06:03:53 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - prints the absolute value of an int
+ *
+ * @a: int
+ *
+ * Return: - id value is less than zero and positive otherwise
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+
+		return (-a);
+
+	else
+		return (a);
+}
