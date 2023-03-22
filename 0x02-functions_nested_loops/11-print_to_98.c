c0c57ef2d70ab8549095057d4411960a91c3966b
gromok-alx <ok.gravin.com>
2023-03-22 13:50:34 -0700

    10-add.c and 11 commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,35 +9,33 @@
 
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-		while (n >= n && n <= 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", n);
+			printf("%d", i);
 
-			if (n < 98)
+			if (i < 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-
-			n++;
 		}
 	}
 
 	else if (n > 98)
 	{
-		while (n <= n && n >= 98)
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d", n);
+			printf("%d", i);
 
-			if (n > 98)
+			if (i > 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-
-			n--;
 		}
 	}
 
