82573c3514f13c4f6a24d493f8ac76f0ec3bb547
gromok-alx <ok.gravin.com>
2023-03-22 10:11:05 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,8 +5,12 @@
 
 void jack_bauer(void)
 {
-	for (int hr = 0; hr <= 23; hr++)
+	int hr;
+
+	for (hr = 0; hr <= 23; hr++)
 	{
+		int mins;
+
 		for (mins = 0; mins <= 59; mins++)
 		{
 			printf("%02d:%02d", hr, mins);
