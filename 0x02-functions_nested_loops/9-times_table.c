a8e99a1cb234812f6db8fbee13bc45d1551a3c49
gromok-alx <ok.gravin.com>
2023-03-22 14:15:59 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * times_table - table of 9
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d ", i*j);
+		}
+
+		printf("\n");
+	}
+}
