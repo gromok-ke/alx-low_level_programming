3e5955eac6d3d3e6df5819b6c772513443519332
gromok-alx <ok.gravin.com>
2023-03-22 07:10:22 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+/**
+ * print_last_digit - prints the last digit of an int value
+ *
+ * @n: int value to be checked
+ *
+ * Return: returns the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}
