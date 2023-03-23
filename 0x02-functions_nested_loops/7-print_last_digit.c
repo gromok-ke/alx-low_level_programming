91e7c4212da332d7bb50eb7f8c8b1cb0e47836af
gromok-alx <ok.gravin.com>
2023-03-23 02:57:39 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - prints the last digit of an int value
@@ -12,7 +12,7 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	_putchar('0' + last_digit);
+	printf("%d", last_digit);
 
 	return (last_digit);
 }
