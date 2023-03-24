a5c960da206cb2d43713cd868f662bc622c115d7
gromok-alx <ok.gravin.com>
2023-03-24 07:01:26 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - checks for uppercase characters
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if True, 0 if False
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0)
+}
