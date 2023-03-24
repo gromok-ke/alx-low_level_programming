a14d06699c8c46d43e36cf65903e338cf398d673
gromok-alx <ok.gravin.com>
2023-03-24 08:07:31 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ *
+ */
+
+void print_most_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+			
+			i++;
+		}
+	}
+	_putchar('\n');
+}
