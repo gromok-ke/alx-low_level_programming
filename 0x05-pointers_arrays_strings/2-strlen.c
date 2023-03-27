72dafe74490807a8689a21491b290b2456952a8a
gromok-alx <ok.gravin.com>
2023-03-27 08:12:42 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the lenght of string value of s
+ *
+ * @s: string variable
+ *
+ * Return: returns the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
