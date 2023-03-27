07124e23ba3f998b6c429a4b560ea5f9be75a492
gromok-alx <ok.gravin.com>
2023-03-27 10:04:55 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ *
+ * @str: string variable
+ */
+
+void puts2(char *str)
+{
+	int len = strlen(str), i;
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
