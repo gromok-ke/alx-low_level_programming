da42d8d341dcb923f3a006076d365339e95774e5
gromok-alx <ok.gravin.com>
2023-03-28 13:41:44 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ *
+ * @str: string variable
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str), i;
+	int half_len = len / 2;
+	int start_index = len % 2 == 0 ? half_len : half_len + 1;
+
+	for (i = start_index; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+}
