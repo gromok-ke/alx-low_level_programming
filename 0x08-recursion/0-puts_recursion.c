b6626171f1e49f56101f2e91e9a1c690f94493a3
gromok-alx <ok.gravin.com>
2023-04-05 06:12:04 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,10 +12,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return ();
 		_putchar('\n');
+		return;
 	}
-
-	_puts_recursion(*s);
-	_putchar(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
