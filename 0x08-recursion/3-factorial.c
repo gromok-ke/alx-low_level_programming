ab9ed1fd763fd2ed61c27a422451a61e56ee0566
gromok-alx <ok.gravin.com>
2023-04-05 07:06:31 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,10 +6,10 @@
  * Return: 1 if *n = 0 else the factorial
  */
 
-int factorial(int *n)
+int factorial(int n)
 {
-	if (*n == 0)
+	if (n == 0)
 		return (1);
 
-	return (*n * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
