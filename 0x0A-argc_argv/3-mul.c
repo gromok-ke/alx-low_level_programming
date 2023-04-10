fe1c9de5ad7fe4a3e7d07c8d7be0a5bb4b1d9ae0
gromok-alx <ok.gravin.com>
2023-04-10 02:43:09 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of command arguments
+ * @argv: command line arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 * num2;
+
+	printf("%d\n", product);
+
+	return(0);
+}
