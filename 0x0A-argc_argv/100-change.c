a2a976c06f68fba31f3aa9b9518d12d0e12d46a1
gromok-alx <ok.gravin.com>
2023-04-11 03:42:10 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * min_coins - returs the min coins
+ *
+ * @cents: cennts to be changed
+ *
+ * Return: always 0
+ */
+
+int min_coins(int cents)
+{
+	int coins = 0;
+
+	if (cents < 0)
+	{
+		printf("0\n");
+
+		return (0);
+	}
+
+	while (cents >= 25)
+	{
+		cents -= 25;
+		coins++;
+	}
+
+	while (cents >= 10)
+	{
+		cents -= 10;
+		coins++;
+	}
+
+	while (cents >= 5)
+	{
+		cents -= 5;
+		coins++;
+	}
+
+	while (cents >= 2)
+	{
+		cents -= 2;
+		coins++;
+	}
+
+	while (cents >= 1)
+	{
+		cents -= 1;
+		coins++;
+	}
+
+	return (coins);
+}
+
+
+/**
+ * main - entry point
+ *
+ * @argc: number of command arguments
+ * @argv: command line arguments
+ *
+ * Return: always 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int coins, cents;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	coins = min_coins(cents);
+
+	printf("%d\n", coins);
+
+	return (0);
+}
