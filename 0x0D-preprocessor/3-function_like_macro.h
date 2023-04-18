09f7626007633a04dcce247c5f01df18371147f6
gromok-alx <ok.gravin.com>
2023-04-18 10:34:46 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number x.
+ * @x: The input number.
+ *
+ * Return: The absolute value of x.
+ */
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /*FUNCTION_LIKE_MACRO_H*/
