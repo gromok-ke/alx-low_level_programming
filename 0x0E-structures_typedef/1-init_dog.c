fa0bb749ef014e653f929f929604a66dd75f9997
gromok-alx <ok.gravin.com>
2023-04-18 08:45:15 -0700

    1-init_dog.c update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,13 +16,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (dog != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-
-	else
-		printf("Ok\n");
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
