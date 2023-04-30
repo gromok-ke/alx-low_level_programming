0dc9a8f17a1f018684a9826ee4ecddd8186eaecb
gromok-alx <ok.gravin@gmail.com>
2023-04-30 02:39:20 -0700

    100 - 103 commit

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe -  frees a listint_t list.
+ *
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	int dif;
+	listint_t *tmp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		dif = *h - (*h)->next;
+		if (dif > 0)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+			count++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			count++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (count);
+}
