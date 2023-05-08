19fc80ef6ffef1a46ac45fad81f6c29c5aeceb31
gromok-alx <ok.gravin@gmail.com>
2023-05-08 06:57:23 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,11 @@
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to
+ * the POSIX standard output.
  * @filename: the name of the file to read.
  * @letters: the number of letters to read and print.
  *
- * Return: If the file cannot be opened or read, return 0. If filename is NULL,
+ * Return: If the file cannot be opened or read, return 0.
+ * If filename is NULL,
  * return 0. If write fails or does not write the expected amount of bytes,
  * return 0. Otherwise, return the actual number of letters read and printed.
  */
